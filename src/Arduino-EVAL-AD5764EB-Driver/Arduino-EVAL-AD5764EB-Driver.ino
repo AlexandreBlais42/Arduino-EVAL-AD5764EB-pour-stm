380a9d78036ae0c32602c2644c7de67aa5870e82
dav1500 <82975501+dav1500@users.noreply.github.com>
2023-10-01 22:22:51 -0400

    Amélioration de l'organisation du code

--- src/Arduino-EVAL-AD5764EB-Driver/Arduino-EVAL-AD5764EB-Driver.ino
+++ src/Arduino-EVAL-AD5764EB-Driver/Arduino-EVAL-AD5764EB-Driver.ino
@@ -1,8 +1,8 @@
 #include <SPI.h>
 #include <stdint.h>
 
-#define DEBUG(txt)  //Serial.print(txt);
-#define DEBUGLN(txt)  //Serial.println(txt);
+#define DEBUG(txt) Serial.print(txt)
+#define DEBUGLN(txt) Serial.println(txt)
 
 const int SYNC_PIN = 10;
 const int LDAC_PIN = 9;
@@ -15,11 +15,9 @@ const uint8_t DATA_REG = 2;
 const uint8_t COARSE_GAIN_REG = 3;
 const uint8_t FINE_GAIN_REG = 4;
 const uint8_t OFFSET_REG = 5;
-
 const uint8_t ALL_DAC = 4;
 
 String command;
-unsigned long timeRef = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -29,7 +27,6 @@ void setup() {
   pinMode(CLR_PIN, OUTPUT);
   pinMode(RSTIN, OUTPUT);
   pinMode(BIN_2COMP, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
 
   digitalWrite(SYNC_PIN, HIGH);
   digitalWrite(LDAC_PIN, LOW);
@@ -40,67 +37,77 @@ void setup() {
   SPI.begin();
   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE1));
 
+  initializeDAC();
+}
+
+void loop() {
+  processSerialInput();
+}
+
+void initializeDAC() {
   writeRegister(FUNCTION_REG, 1, 0x00);
   writeRegister(COARSE_GAIN_REG, ALL_DAC, 0x00);
   writeRegister(OFFSET_REG, ALL_DAC, 0x00);
 }
 
-void loop() {
-  
+void processSerialInput() {
   while (Serial.available()) {
-  
     char rx = Serial.read();
-    
-    if ((rx == '\r') || (rx == '\n')) {
+
+    if (rx == '\r' || rx == '\n') {
       if (command.length() != 0) {
         command.trim();
 
         DEBUGLN("---------------");
         DEBUGLN(command);
-        
-        char ch = command.charAt(0);
 
-        DEBUG("ch: ");
-        DEBUGLN(ch);
-        
-        if (ch >= 'a' && ch <= 'd') {
-          ch -= 'a';
-        } else if (ch >= 'A' && ch <= 'D') {
-          ch -= 'A';
-        } else if (ch >= '0' && ch <= '3') {
-          ch -= '0';
-        }
+        processCommand(command);
 
-        command.remove(0, 1);
-        char valeur[4];
-        command.toCharArray(valeur, 5);
-        long valeurdecimale = strtol(valeur, NULL, 16);
-
-        DEBUG("V: ");
-        DEBUGLN(V);
-
-        long bin = (valeurdecimale / 2) + 32768;
-
-        DEBUG("bin: ");
-        DEBUGLN(bin);
-
-        writeRegister(DATA_REG, ch, bin);
+        // Acknowledge receipt of the command (optional)
+        Serial.println("Command received: " + command);
       }
       command = "";
     } else {
       command.concat(rx);
     }
   }
-
-  unsigned long now = millis();
-  if ((now - timeRef) >= 1000) {
-    timeRef = now;
-    digitalWrite( LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  }
 }
 
-void writeRegister( uint8_t function, uint8_t addr, int16_t regValue) {
+void processCommand(const String& command) {
+  char ch = command.charAt(0);
 
+  DEBUG("ch: ");
+  DEBUGLN(ch);
+
+  if (ch >= 'a' && ch <= 'd') {
+    ch -= 'a';
+  } else if (ch >= 'A' && ch <= 'D') {
+    ch -= 'A';
+  } else if (ch >= '0' && ch <= '3') {
+    ch -= '0';
+  } else {
+    // Handle invalid command
+    DEBUGLN("Invalid command");
+    return;
+  }
+
+  command.remove(0, 1);
+  char valeur[5];
+  command.toCharArray(valeur, 5);
+  long valeurdecimale = strtol(valeur, NULL, 16);
+
+  DEBUG("V: ");
+  DEBUGLN(valeurdecimale);
+
+  long bin = (valeurdecimale / 2) + 32768;
+
+  DEBUG("bin: ");
+  DEBUGLN(bin);
+
+  writeRegister(DATA_REG, ch, bin);
+}
+
+void writeRegister(uint8_t function, uint8_t addr, int16_t regValue) {
   function &= 0x07;
   addr &= 0x07;
 
